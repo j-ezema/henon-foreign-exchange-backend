5cdd4ace047d481277c7facb64753c7c276db47c
jezem009 <j.ezema01@gmail.com>
2024-06-11 00:20:35 -0400

    Set up static file serving

--- henon_fx_tracker/henon_fx_tracker/settings.py
+++ henon_fx_tracker/henon_fx_tracker/settings.py
@@ -133,6 +133,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
