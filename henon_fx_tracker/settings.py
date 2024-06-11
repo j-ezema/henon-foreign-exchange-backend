064a639bc0ce5872c41d92c8e107392760df4346
jezem009 <j.ezema01@gmail.com>
2024-06-11 01:49:24 -0400

    Update settings.py

--- henon_fx_tracker/settings.py
+++ henon_fx_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+ju)#)owvh-a!n(qjw%5u4lo%o74uuvk*1t305hu870a&$lx(*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["henon-fx-tracker-app-679990af8b3b.herokuapp.com"]
 
 
 # Application definition
@@ -106,6 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "henon-fx-tracker-app-679990af8b3b.herokuapp.com"
 ]
 
 # Internationalization
